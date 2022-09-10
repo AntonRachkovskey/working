709530f2308528b640300cce7b338995f58b5d3b
zts19 <zankovets19@mail.ru>
2022-09-10 22:25:47 +0300

    completed app section

--- index.js
+++ index.js
@@ -5,10 +5,30 @@ const field = document.querySelector('.app__field');
 const fieldHeight = field.getBoundingClientRect().height;
 const fieldWidth = field.getBoundingClientRect().width;
 
+let scoreCounter = 0;
+
 function getRandomNumber (max) {
-    return Matn.floor(Math.random()* max);
+    let randomNumber = Math.floor(Math.random() * max);
+    if (randomNumber > max - 70){
+        return randomNumber - 70
+    } else {
+        return randomNumber
+    }
 }
 
 zero.addEventListener('mouseenter', () => {
-    // zero.style.top = 
+    setTimeout(() => {
+        zero.style.top = `${getRandomNumber(fieldHeight)}px`;
+        zero.style.left = `${getRandomNumber(fieldWidth)}px`;
+        zero.style.transform = 'none';
+    }, 150)   
+})
+
+zero.addEventListener('click', () => {
+    field.style.backgroundColor = 'red';
+    setTimeout(() => {
+        field.style.backgroundColor = 'black'
+    }, 100)
+    scoreCounter++
+    score.textContent = scoreCounter;
 })
